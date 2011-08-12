11df6396c901d6149a2f66892421dc6f82bd18c8
John Flatness <john@zerocrates.org>
2011-08-12 12:23:19 -0400

    Fix new bug introduced by last commit.
    That moved the "break" too far out, meaning that only the first route
    was checked. Third time's the charm.

--- libraries/SimpleVocab/Controller/Plugin/SelectFilter.php
+++ libraries/SimpleVocab/Controller/Plugin/SelectFilter.php
@@ -41,11 +41,11 @@ class SimpleVocab_Controller_Plugin_SelectFilter extends Zend_Controller_Plugin_
                                      $element->name), 
                                array($this, 'filterElement'));
                 }
+                // Once the filter is applied for one action it is applied
+                // for all subsequent actions, so there is no need to
+                // continue looping the routes.
+                break;
             }
-            // Once the filter is applied for one action it is applied
-            // for all subsequent actions, so there is no need to
-            // continue looping the routes.
-            break;
         }
     }
     
