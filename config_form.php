6beaea2e0d5d38a30958b7f50a71b186c14b3bea
John Flatness <john@zerocrates.org>
2023-04-17 14:58:26 -0400

    Fix admin styling

--- config_form.php
+++ config_form.php
@@ -1,20 +1,18 @@
-<section class="seven columns alpha">
-    <div class="field">
-        <div class="two column alpha">
-            <label for="simple_vocab_files">Apply to Files</label>
-        </div>
-        <div class="inputs five columns omega">
-            <?php echo $view->formCheckbox(
-                'simple_vocab_files',
-                null,
-                array(
-                    'id' => 'simple_vocab_files',
-                    'checked' => (bool) get_option('simple_vocab_files'),
-                )
-            ); ?>
-            <p class="explanation">By default, your simple vocabularies will only
-            apply to item metadata. Check the above box if you want to apply your
-            vocabularies to file metadata as well.</p>
-        </div>
+<div class="field">
+    <div class="field-meta">
+        <label for="simple_vocab_files">Apply to Files</label>
     </div>
-</section>
+    <div class="inputs">
+        <?php echo $view->formCheckbox(
+            'simple_vocab_files',
+            null,
+            array(
+                'id' => 'simple_vocab_files',
+                'checked' => (bool) get_option('simple_vocab_files'),
+            )
+        ); ?>
+        <p class="explanation">By default, your simple vocabularies will only
+        apply to item metadata. Check the above box if you want to apply your
+        vocabularies to file metadata as well.</p>
+    </div>
+</div>
