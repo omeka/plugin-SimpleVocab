8d4efe4c4ac6e1405ec3f6f4e855223d5c8add2c
Erin Bell <ebellempire@gmail.com>
2023-12-13 10:34:13 -0500

    remove unnecessary str_replace

--- views/admin/index/element-texts.ajax.php
+++ views/admin/index/element-texts.ajax.php
@@ -13,7 +13,7 @@
         <td class="error"><?php echo implode("<br />", $element_text['warnings']); ?></td>
         <td>
         <?php if(!get_option('simple_vocab_files')):?>
-            <a target="blank" href="<?php echo html_escape(url('items/browse?search=&advanced[0][joiner]=and&advanced[0][element_id]='.$element_text['element_id'].'&advanced[0][type]=is+exactly&advanced[0][terms]='.urlencode(str_replace('\n','%0D%0A',$element_text['text'])))); ?>"><?php echo snippet(nl2br($element_text['text']), 0, 600); ?></a>
+            <a target="blank" href="<?php echo html_escape(url('items/browse?search=&advanced[0][joiner]=and&advanced[0][element_id]='.$element_text['element_id'].'&advanced[0][type]=is+exactly&advanced[0][terms]='.urlencode($element_text['text']))); ?>"><?php echo snippet(nl2br($element_text['text']), 0, 600); ?></a>
         <?php else:?>
             <?php echo snippet(nl2br($element_text['text']), 0, 600); ?>
         <?php endif;?>
