488a516bef0aa10350e7f8e15128d1fd80700262
Erin Bell <ebellempire@gmail.com>
2024-08-28 12:33:36 -0400

    code style

--- views/admin/index/element-texts.ajax.php
+++ views/admin/index/element-texts.ajax.php
@@ -12,13 +12,12 @@
         <td><?php echo $element_text['count']; ?></td>
         <td class="error"><?php echo implode("<br />", $element_text['warnings']); ?></td>
         <td>
-        <?php if(!get_option('simple_vocab_files')):?>
             <a target="blank" href="<?php echo html_escape(url('items/browse?search=&advanced[0][joiner]=and&advanced[0][element_id]='.$element_text['element_id'].'&advanced[0][type]=is+exactly&advanced[0][terms]='.urlencode($element_text['text']))); ?>"><?php echo snippet(nl2br($element_text['text']), 0, 600); ?></a>
-        <?php else:?>
+        <?php if (!get_option('simple_vocab_files')): ?>
+        <?php else: ?>
             <?php echo snippet(nl2br($element_text['text']), 0, 600); ?>
-        <?php endif;?>
+        <?php endif; ?>
         </td>
-        
     </tr>
     <?php endforeach; ?>
 </table>
